8e99a3326fab2d4dce4157c3710c7af66757d4a7
Stephen Ayoola <stephen@securebroadcast.com>
2021-03-05 16:16:39 +0000

    Home page setup- router added to create pages

--- totalmoney/src/App.js
+++ totalmoney/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Steve test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import  CardType  from "./components/CardType.js";
+// import  DataForm  from "./components/DataForm.js";
+// import  CardResult  from "./components/CardResult.js";
+import "./App.scss"
+// import {A} from "hookrouter"
 
-export default App;
+const Home = lazy(() => import('./Home'))
+const CardType = lazy(() => import('./components/CardType'))
+const Form = lazy(() => import('./components/DataForm'))
+const Forms= lazy(() => import('./components/DataForms'))
+const Result = lazy(() => import('./components/CardResult'))
+
+// /components/CardType
+
+const App = () => (
+    <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/CardType" component={CardType}/>
+            <Route path="/form" component={Form}/>
+            <Route path="/forms" component={Forms}/>
+            <Route path="/result" component={Result}/>
+        </Switch>
+        </Suspense>
+    </Router>
+)
+   
+
+    // return (
+    //     <>
+    //         <div className="navbar"> 
+    //             <A className="nav_link" href="/"> Home</A>
+    //             <A className="nav_link" href="/form"> Form</A>
+    //             <A className="nav_link" href="/result"> Result</A>
+    //         </div>
+    //         <div className="container_main"> 
+    //             <div className="product_main">
+    //                 <div className="product_name"> 
+    //                     <h1 className= "header_main"> Welcome to Total Money, please select a card of your choice </h1>
+    //                 </div>
+    //                 <div> 
+    //                     <CardType balanceDuration = "28" offerDuration = "3" aprvalue = "21.9" transferFee = "2.7" />
+    //                     <CardType balanceDuration = "24" offerDuration = "6" aprvalue = "23.9" transferFee = "2.7" />
+    //                     <CardType balanceDuration = "12" offerDuration = "3" aprvalue = "25.9" transferFee = "2.7" />
+    //                 </div>
+    //                 <div className="form_main"> 
+    //                     <DataForm/>
+    //                 </div>
+    //                 <div className="form_main"> 
+    //                     <CardResult/>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </>
+    // )
+//  }
+
+export default App
